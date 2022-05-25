d0d35c6712825b34cbb0b1a36f0e9c9c42be3ea0
tomisin110 <54535614+tomisin110@users.noreply.github.com>
2022-05-25 09:35:46 +0100

    comitted

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react"
+import pauseMobile from "./images/pattern-divider-mobile.svg"
+import pauseDesktop from "./images/pattern-divider-desktop.svg"
+import dice from "./images/icon-dice.svg"
+
+function App() {
+  const [text, setText] = useState([])
+
+  const fetchAdvice = async () => {
+    const res = await fetch("https://api.adviceslip.com/advice")
+    const data = await res.json()
+
+    console.log(data)
+
+    setText(data.slip)
+  }
+
+  useEffect(() => {
+    fetchAdvice()
+  }, [])
+
+  return (
+    <div className="container">
+      <h1>Advice #{text.id}</h1>
+      <p>{text.advice}</p>
+
+      <picture>
+        <source media="(min-width: 768px)" srcSet={pauseDesktop} />
+        <img src={pauseMobile} alt="" />
+      </picture>
+
+      <div>
+        <button onClick={fetchAdvice}>
+          <img src={dice} alt="" />
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
